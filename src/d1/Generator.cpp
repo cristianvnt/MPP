d8233ae0866e2b1313bf555ea09a07b19de43cc6
Mematoru <mematoru@gmail.com>
2026-03-13 08:56:21 +0200

    optional seed arg

--- src/d1/Generator.cpp
+++ src/d1/Generator.cpp
@@ -6,16 +6,16 @@
 
 int main(int argc, char** argv)
 {
-    if (argc < 5)
+    if (argc < 4)
     {
-        std::cerr << "Use: " << argv[0] << " <M> <outputFile> <text | binary> <seed>\n";
+        std::cerr << "Use: " << argv[0] << " <M> <outputFile> <text | binary> [seed]\n";
         return 1;
     }
 
     size_t M = std::stoul(argv[1]);
     std::string filename = argv[2];
     bool binary = (std::strcmp(argv[3], "binary") == 0);
-    uint32 seed = static_cast<uint32>(std::stoul(argv[4]));
+    uint32 seed = (argc > 4) ? static_cast<uint32>(std::stoul(argv[4])) : 67;
 
     std::mt19937 rng(seed);
     std::uniform_real_distribution<double> dist(-100.0, 100.0);
